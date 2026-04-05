b7ba321826df386f89a4207c7f8242f10140a956
Mohd Aadil <pqradil051@gmail.com>
2026-04-05 20:04:42 +0530

    Implement peak index finder for mountain array

---
+++ Arrays/peakIndex_Mountain_array.java
@@ -0,0 +1,35 @@
+//problem-peak index in moutain Array
+//platform-leetcode/852
+//difficulty-Medium
+// Approach:
+// simple binary search to find the target no. in the Arrays and print its index
+
+
+
+
+
+
+
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid=0;
+        while(start<end){
+            mid=start+(end-start)/2;
+            if(arr[mid]<arr[mid+1]){
+                start=mid+1;
+            }
+            
+            else{
+                end=mid;
+            }
+
+        }
+        return start;
+
+    
+        
+    }
+}
